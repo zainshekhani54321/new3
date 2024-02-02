3b74efac29ec6716d7b51975f25a088d9d4b2b65
zainshekhani54321 <157803688+zainshekhani54321@users.noreply.github.com>
2024-02-03 00:14:59 +0500

    Add files via upload

---
+++ result.php
@@ -0,0 +1,129 @@
+<?php
+    if ($_POST["name"] == "search him") {
+        ?><a href="index.html">Welcome To <i>"ZAIN SHEKHANI"</i> Project {Click Me}....</a> <?php
+    }
+
+     if ($_POST["name"] == "naats") {
+        ?> <iframe width="560" height="315" src="https://www.youtube.com/embed/zBHvqEDO4RA?si=fkv1djSVnyXYRj7k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe><?php       
+          
+    }
+
+
+
+
+?>
+
+<?php
+
+
+        if ($_POST["name"] == "naats") {
+        ?><iframe width="560" height="315" src="https://www.youtube.com/embed/NAyqY-jyZCg?si=YWx9xILyktdLb6u8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe><?php
+
+
+      }
+
+
+
+?>
+
+<?php
+
+
+
+  if ($_POST["name"] == "naats") {
+        ?><iframe width="560" height="315" src="https://www.youtube.com/embed/R6kR8BWw-3E?si=IXqERhcCQHtkB3Ax" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe><?php
+
+
+      }
+
+?>
+
+
+
+<?php
+
+
+  if ($_POST["name"] == "naats") {
+        ?><iframe width="560" height="315" src="https://www.youtube.com/embed/L7dCk09AwWs?si=ZyskqWFHUTKEn_r6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe><?php
+
+
+      }
+
+  ?>
+
+  <?php
+
+
+   if ($_POST["name"] == "cars") {
+        ?><iframe width="560" height="315" src="https://www.youtube.com/embed/7kpoOOItWIg?si=94DPzpCMdvU_Hh31" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe><?php
+      }
+
+  ?>
+
+  <?php
+
+
+
+   if ($_POST["name"] == "cars") {
+       ?> <iframe width="560" height="315" src="https://www.youtube.com/embed/XCu7DsunC94?si=B6ZCTIVcPlLB_v1w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe><?php
+      }
+
+
+
+
+?>
+
+
+
+  <?php
+
+
+
+   if ($_POST["name"] == "cars") {
+       ?> <iframe width="560" height="315" src="https://www.youtube.com/embed/o7pf4ITEBQ0?si=9JNRuppneoNwrkNV" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe><?php
+      }
+       
+
+
+
+?>  <?php
+
+
+
+   if ($_POST["name"] == "cars") {
+       ?><iframe width="560" height="315" src="https://www.youtube.com/embed/rRrqkjYGURw?si=FcApeI_ENZ72biBz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> <?php
+      }
+       
+
+
+
+?>
+
+
+
+?>  <?php
+
+
+
+   if ($_POST["name"] == "img") {
+       ?><iframe src="img10.jpeg" width="900px"
+       height="900px"></iframe><?php
+      }
+       
+
+
+
+?>
+
+          <?php
+
+
+
+   if ($_POST["name"] == "search him") {
+       ?><br><a href="car1.jpeg">car</a> <?php
+      }
+       
+
+
+
+?>
